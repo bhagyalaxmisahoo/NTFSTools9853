7a2ea71cdc05167b3c2481159705b22d46eb0fe9
HP <HP@LAPTOP-3NFP0DOQ>
2025-01-29 23:32:54 +0530

    TL change 44

--- GITProj01/src/main/java/com/nit/main/App.java
+++ GITProj01/src/main/java/com/nit/main/App.java
@@ -8,8 +8,8 @@ public class App {
 	}
     public static void main(String[] args) {
         App app=new App();
-    System.out.println("sum result22 is ::"+app.sum(10, 20));
-     System.out.println("sum amount33  is::"+app.sum(10, 20));
-        
+    System.out.println("sum result22 is22 ::"+app.sum(10, 20));
+    System.out.println("sum amount33  is44::"+app.sum(10, 20));
+      //hello  
     }
 }
