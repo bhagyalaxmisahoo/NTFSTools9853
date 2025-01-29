b9e5a3d8803c4f8ec029ec0b73990933e4533edd
HP <HP@LAPTOP-3NFP0DOQ>
2025-01-29 17:24:18 +0530

    TL sum result change

--- GITProj01/src/main/java/com/nit/main/App.java
+++ GITProj01/src/main/java/com/nit/main/App.java
@@ -1,17 +1,13 @@
 package com.nit.main;
 
-/**
- * Hello world!
- */
 public class App {
-	
-	
+
 	public int sum(int x,int y)
 	{
 		return x+y;
 	}
     public static void main(String[] args) {
         App app=new App();
-        System.out.println("Sum  amount333 is::"+app.sum(10,20));
+        System.out.println("Sum amount11 is::"+app.sum(10, 20));
     }
 }
