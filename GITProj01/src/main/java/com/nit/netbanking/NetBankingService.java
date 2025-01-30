d90a21b23eaedde4e3dcef6a0b72bfd29d22fe31
HP <HP@LAPTOP-3NFP0DOQ>
2025-01-30 14:01:21 +0530

    dev1 ---account opening logic

--- GITProj01/src/main/java/com/nit/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nit/netbanking/NetBankingService.java
@@ -1,5 +1,11 @@
 package com.nit.netbanking;
 
-public class NetBankingService {
+import java.util.Random;
 
+public class NetBankingService 
+{
+	public String openAccount(String aadhar,double initialamount,String type)
+	{
+		return "account is opened with acc number:: "+new Random().nextLong(28787678787443L);
+	}
 }
